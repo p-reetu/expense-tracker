f5b13695be83692ba7d76dc3ce3427e598664a33
p-reetu <79264123+p-reetu@users.noreply.github.com>
2021-12-30 12:55:07 +0530

    responsiveness added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NewExpense from "./components/NewExpense";
 const dummy_list = [
   {
     id: "e1",
-    title: "Toilet Paper",
+    title: "Paper",
     amount: 94.12,
     date: new Date(2020, 7, 14)
   },
